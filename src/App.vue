1fec414b968282d6f832e40cf15cc2811bca49bb
surViVeCZ <poucpetr11@seznam.cz>
2021-10-03 20:07:15 +0200

    top backround

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,7 @@
       >
         <a
           class="
+            h-24
             text-sm
             font-bold
             leading-relaxed
@@ -41,7 +42,7 @@
           "
           href="#pablo"
         >
-          Tady bude logo
+          <img src="resources/logo.png" style="float:left;width:100px;height:100px;">
         </a>
         <button
           class="
@@ -78,68 +79,106 @@
               aria-label=" Search"
             />
             <button
-              class="fab fa-twitter text-lg leading-lg text-white opacity-75"
+              class="font-bold py-2 px-2 uppercase text-xs text-white hover:opacity-75"
               type="submit"
             >
               Potvrdit
             </button>
           </form>
-          <li class="nav-item">
-            <a
-              class="
-                px-3
-                py-2
+
+          
+          <div class="fab
                 flex
-                items-center
+                items-upper
                 text-xs
                 uppercase
                 font-bold
                 leading-snug
                 text-white
                 hover:opacity-75
-              "
-              href="#pablo"
-            >
-              <i
-                class="
-                  fab
-                  fa-facebook-square
-                  text-lg
-                  leading-lg
-                  text-white
-                  opacity-75
-                "
-              /><span class="ml-2">Sledované aukce</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="
-                px-3
-                py-2
+                block px-4 py-2 
+                rounded-lg 
+                dark-mode:focus:text-white 
+                hover:text-gray-900 
+                focus:text-gray-900 
+                hover:bg-gray-200 
+                focus:bg-gray-200 
+                focus:outline-none
+                overflow-visible" 
+                href="#">
+                Contact</div>
+    
+            <div class="fab
                 flex
-                items-center
+                items-upper
                 text-xs
                 uppercase
                 font-bold
                 leading-snug
                 text-white
                 hover:opacity-75
-              "
-              href="#pablo"
-            >
-              <i
-                class="fab fa-twitter text-lg leading-lg text-white opacity-75"
-              /><span class="ml-2">Registrace</span>
-            </a>
-          </li>
-          <li class="nav-item">
+                block px-4 py-2 
+                rounded-lg 
+                dark-mode:focus:text-white 
+                hover:text-gray-900 
+                focus:text-gray-900 
+                hover:bg-gray-200 
+                focus:bg-gray-200 
+                focus:outline-none
+                overflow-visible" 
+                href="#">
+                Sledované aukce</div>
+
+                
+           <div class="fab
+                flex
+                items-upper
+                text-xs
+                uppercase
+                font-bold
+                leading-snug
+                text-white
+                hover:opacity-75
+                block px-4 py-2 
+                rounded-lg 
+                dark-mode:focus:text-white 
+                hover:text-gray-900 
+                focus:text-gray-900 
+                hover:bg-gray-200 
+                focus:bg-gray-200 
+                focus:outline-none
+                overflow-visible" 
+                href="#">
+                Registrace</div>
+          
+
+             <div class="fab
+                flex
+                items-upper
+                text-xs
+                uppercase
+                font-bold
+                leading-snug
+                text-white
+                hover:opacity-75
+                block px-4 py-2 
+                rounded-lg 
+                dark-mode:focus:text-white 
+                hover:text-gray-900 
+                focus:text-gray-900 
+                hover:bg-gray-200 
+                focus:bg-gray-200 
+                focus:outline-none
+                overflow-visible" 
+                href="#">
+                Přihlášení/Registrace</div>
+          <!-- <li class="nav-item">
             <a
               class="
                 px-3
                 py-2
                 flex
-                items-center
+                items-right
                 text-xs
                 uppercase
                 font-bold
@@ -158,14 +197,16 @@
                   text-white
                   opacity-75
                 "
-              /><span class="ml-2">Přihlášení</span>
+              />
+              <span class="flex justify-between">Přihlášení</span><svg class="fill-current text-gray-200" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"></svg> 
+                      <div x-show="show" class="mt-2 py-2 bg-white rounded-lg shadow-xl"> <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Settings</a> <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Profile</a> <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Signout</a> </div>
+
             </a>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
   </nav>
-
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link> |
@@ -175,11 +216,15 @@
     <router-link :to="{ name: 'Login' }">Login</router-link>
   </div>
   <router-view />
+
+  
 </template>
 
 
+
+
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -202,5 +247,5 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
-}
+} */
 </style>
