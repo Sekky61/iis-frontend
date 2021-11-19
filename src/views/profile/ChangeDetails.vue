98e9524b47426e0ad79ffe15c6c6285d9e13a0ef
Sekky61 <misa@majer.cz>
2021-11-19 17:33:23 +0100

    createauction

--- src/views/profile/ChangeDetails.vue
+++ src/views/profile/ChangeDetails.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-export default {};
+export default {}; // todo
 </script>
 
 <style>
--- src/views/user/CreateAuction.vue
+++ src/views/user/CreateAuction.vue
@@ -1,90 +1,77 @@
 <template>
-  <h1>Vytvořit novou aukci</h1>
+  <h1 class="text-2xl m-2">Vytvořit novou aukci</h1>
 
-  <form
-    id="new_auction_form"
-    @submit.prevent="processForm"
-    @keyup.enter="processForm"
-    class="m-8"
-  >
-    <input-field
-      class="w-full"
-      v-bind="auction_name_field"
-      @fieldchange="change_field(auction_name_field, $event)"
-    >
-    </input-field>
+  <form id="new_auction_form" @submit.prevent="processForm" class="m-8">
+    <label class="text-sm font-bold pl-1">Název aukce</label>
+    <input
+      v-model="auction_name"
+      class="input-field"
+      type="text"
+      placeholder="Zahrada 300m2"
+      @blur.capture="validate_auction_name"
+      :class="{ 'input-incorrect': !auction_name_valid }"
+    />
+    <label class="text-sm font-bold pl-1">Počáteční cena</label>
+    <input
+      v-model="starting_price"
+      class="input-field"
+      type="text"
+      placeholder="150000"
+      @blur.capture="validate_starting_price"
+      :class="{ 'input-incorrect': !starting_price_valid }"
+    />
 
-    <input-field
-      class="w-full"
-      v-bind="starting_price_field"
-      @fieldchange="change_field(starting_price_field, $event)"
-    >
-    </input-field>
+    <label class="text-sm font-bold pl-1">Minimální výše příhozu</label>
+    <input
+      v-model="min_bid"
+      class="input-field"
+      type="text"
+      placeholder="5000"
+      @blur.capture="validate_min_bid"
+      :class="{ 'input-incorrect': !min_bid_valid }"
+    />
 
-    <input-field
-      class="w-full"
-      v-bind="min_bid_field"
-      @fieldchange="change_field(min_bid_field, $event)"
-    >
-    </input-field>
-
-    <label class="text-grey-darker text-sm font-bold pl-1">{{
-      rule_field.label
-    }}</label>
+    <label class="text-sm font-bold pl-1">Pravidlo aukce</label>
     <select
-      v-model="rule_field.value"
-      class="
-        focus:outline-none
-        appearance-none
-        rounded
-        w-full
-        mt-1
-        py-2
-        px-3
-        text-grey-darker
-        bg-white
-        border
-      "
+      @blur.capture="validate_rule"
+      :class="{ 'input-incorrect': !rule_valid }"
+      v-model="rule"
+      class="input-field"
     >
       <option value="uzavrena">Uzavřená</option>
       <option value="otevrena">Otevřená</option>
     </select>
 
-    <label class="text-grey-darker text-sm font-bold pl-1">{{
-      type_field.label
-    }}</label>
+    <label class="text-sm font-bold pl-1">Typ aukce</label>
     <select
-      v-model="type_field.value"
-      class="
-        focus:outline-none
-        appearance-none
-        rounded
-        w-full
-        mt-1
-        py-2
-        px-3
-        text-grey-darker
-        bg-white
-        border
-      "
+      @blur.capture="validate_type"
+      :class="{ 'input-incorrect': !type_valid }"
+      v-model="type"
+      class="input-field"
     >
       <option value="nabidkova">Nabídková</option>
       <option value="poptavkova">Poptávková</option>
     </select>
 
-    <input-field
-      class="w-full"
-      v-bind="min_participants_field"
-      @fieldchange="change_field(min_participants_field, $event)"
-    >
-    </input-field>
+    <label class="text-sm font-bold pl-1">Minimální počet účastníků</label>
+    <input
+      v-model="min_participants"
+      class="input-field"
+      type="text"
+      placeholder="1"
+      @blur.capture="validate_min_participants"
+      :class="{ 'input-incorrect': !min_participants_valid }"
+    />
 
-    <input-field
-      class="w-full"
-      v-bind="object_field"
-      @fieldchange="change_field(object_field, $event)"
-    >
-    </input-field>
+    <label class="text-sm font-bold pl-1">Číslo objektu</label>
+    <input
+      v-model="object"
+      class="input-field"
+      type="text"
+      placeholder="např. 11"
+      @blur.capture="validate_object"
+      :class="{ 'input-incorrect': !object_valid }"
+    />
 
     <div class="flex items-center justify-center my-4">
       <submit-button>Založit aukci</submit-button>
@@ -101,93 +88,91 @@ export default {
   components: { InputField, SubmitButton },
   data() {
     return {
-      auction_name_field: {
-        label: "Název aukce",
-        placeholder: "Prodej bytu 3+kk 86m2",
-        value: "",
-        type: "text",
-      },
+      // auction_name, starting_price, min_bid, rule, type, min_participants, object
 
-      starting_price_field: {
-        label: "Startovací cena",
-        placeholder: "1010000",
-        value: "",
-        type: "text",
-      },
+      auction_name: "",
+      starting_price: "",
+      min_bid: "",
+      rule: "",
+      type: "",
+      min_participants: "",
+      object: "",
 
-      min_bid_field: {
-        label: "Minimální příhoz",
-        placeholder: "5000",
-        value: "",
-        type: "text",
-      },
-
-      rule_field: {
-        label: "Pravidlo aukce",
-        placeholder: "",
-        value: "",
-        type: "text",
-      },
-
-      type_field: {
-        label: "Typ aukce",
-        placeholder: "",
-        value: "",
-        type: "text",
-      },
-
-      min_participants_field: {
-        label: "Minimální počet účastníků",
-        placeholder: "např. 3",
-        value: "",
-        type: "text",
-      },
-
-      object_field: {
-        label: "Číslo objektu",
-        placeholder: "např. 2",
-        value: "",
-        type: "text",
-      },
+      auction_name_valid: true,
+      starting_price_valid: true,
+      min_bid_valid: true,
+      rule_valid: true,
+      type_valid: true,
+      min_participants_valid: true,
+      object_valid: true,
     };
   },
 
-  computed: {
-    pravidlo_converted() {
-      if (this.rule_field.value == "Uzavřená") {
-        return "uzavrena";
-      } else if (this.rule_field.value == "Otevřená") {
-        return "otevrena";
-      }
-      return "undef";
-    },
-
-    typ_converted() {
-      if (this.rule_field.value == "Nabídková") {
-        return "uzavrena";
-      } else if (this.rule_field.value == "Poptávková") {
-        return "poptavkova";
-      }
-      return "undef";
-    },
-  },
+  computed: {},
 
   methods: {
     ...mapActions(["create_auction", "new_notif"]),
 
-    change_field(obj, new_val) {
-      obj.value = new_val;
+    validate_auction_name() {
+      this.auction_name_valid = true;
+    },
+    validate_starting_price() {
+      this.starting_price_valid = true;
+    },
+    validate_min_bid() {
+      this.min_bid_valid = true;
+    },
+    validate_rule() {
+      this.rule_valid = true;
+    },
+    validate_type() {
+      this.type_valid = true;
+    },
+    validate_min_participants() {
+      this.min_participants_valid = true;
+    },
+    validate_object() {
+      this.object_valid = true;
+    },
+
+    form_valid() {
+      this.validate_auction_name();
+      this.validate_starting_price();
+      this.validate_min_bid();
+      this.validate_rule();
+      this.validate_type();
+      this.validate_min_participants();
+      this.validate_object();
+
+      return (
+        this.auction_name_valid &&
+        this.starting_price_valid &&
+        this.min_bid_valid &&
+        this.rule_valid &&
+        this.type_valid &&
+        this.min_participants_valid &&
+        this.object_valid
+      );
     },
 
     async processForm() {
+      const valid = form_valid();
+      if (!valid) {
+        this.new_notif({
+          text: "Špatné vstupy", // todo exact messages
+          urgency: "error",
+        });
+        return;
+      }
+
       let form_data = {
-        nazev: this.auction_name_field.value,
-        vyvolavaci_cena: this.starting_price_field.value,
-        min_prihoz: this.min_bid_field.value,
-        pravidlo: this.rule_field.value,
-        typ: this.type_field.value,
-        min_ucastniku: this.min_participants_field.value,
-        object: this.object_field.value,
+        nazev: this.auction_name,
+        vyvolavaci_cena: this.starting_price,
+        min_prihoz: this.min_bid,
+        pravidlo: this.rule,
+        typ: this.type,
+        min_ucastniku: this.min_participants,
+        object: this.object,
       };
 
       const response = await this.create_auction(form_data);
